7db768039a7716d4b58c8e835153f7da7f4acfd8
lightkira2 <lilliu8davide@gmail.com>
2025-12-11 21:28:26 +0100

    Update climate.py

--- custom_components/myhome/climate.py
+++ custom_components/myhome/climate.py
@@ -20,7 +20,7 @@ from homeassistant.const import (
     UnitOfTemperature,
 )
 
-from OWNd.message import (
+from .own_wrapper import (
     OWNHeatingEvent,
     OWNHeatingCommand,
     CLIMATE_MODE_OFF,
